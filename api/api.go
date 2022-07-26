02785014609a63976088e092216a53586a4bafef
Dikdik <dikdik.kurniawan.bdg@gmail.com>
2022-07-26 17:46:02 +0700

    go dev

--- api/api.go
+++ api/api.go
@@ -3,15 +3,16 @@ package api
 import (
 	"crypto-project/helpers"
 	"crypto-project/interfaces"
-	"crypto-project/users"
-	"crypto-project/posts"
 	"crypto-project/jadwals"
+	"crypto-project/posts"
+	"crypto-project/users"
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 type JSONResponse struct {
@@ -31,48 +32,57 @@ type Login struct {
 }
 
 type Register struct {
-	Username 	string
-	Email    	string
-	Password 	string
-	User_type 	string
+	Username  string
+	Email     string
+	Password  string
+	User_type string
 }
 
 type User struct {
-	Username 	string
-	Password 	string
-	Email 		string
-	UserType	string
-	UserID 		string
-	Balance		uint
-	Name		string
+	Username string
+	Password string
+	Email    string
+	UserType string
+	UserID   string
+	Balance  uint
+	Name     string
+}
+
+type Pengguna struct {
+	ID       int8   `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	UserType string `json:"user_type"`
+	Nama     string `json:"nama"`
 }
 
 type Post struct {
-	User_ID 	uint
-	Name    	string
-	Skill 		string
-	Location 	string
-	Position 	string
-	Work 		string
-	Salary 		uint
-	Message 	string
+	User_ID  uint
+	Name     string
+	Skill    string
+	Location string
+	Position string
+	Work     string
+	Salary   uint
+	Message  string
 }
 
 type UpdatePost struct {
-	ID 			uint
-	User_ID 	uint
-	Name    	string
-	Skill 		string
-	Location 	string
-	Position 	string
-	Work 		string
-	Salary 		uint
-	Message 	string
+	ID       uint
+	User_ID  uint
+	Name     string
+	Skill    string
+	Location string
+	Position string
+	Work     string
+	Salary   uint
+	Message  string
 }
 
 type Jadwal struct {
-	Kuota 	uint
-	Lokasi 	string
+	Kuota  uint
+	Lokasi string
 }
 
 func RespondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -90,7 +100,6 @@ func RespondWithError(w http.ResponseWriter, code int, message string) {
 	json := ErrorResponse{Status: "Failed", Message: message}
 	RespondWithJSON(w, code, json)
 }
-
 
 func readBody(r *http.Request) []byte {
 	body, err := ioutil.ReadAll(r.Body)
@@ -152,15 +161,15 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 func readAllUser(w http.ResponseWriter, r *http.Request) {
 
 	//TODO: Gunakan apiresponse interfasce
-	auth 	:= r.Header.Get("Authorization")	
-	userID 	:= helpers.UserIDStr(auth)
+	auth := r.Header.Get("Authorization")
+	userID := helpers.UserIDStr(auth)
 	isValid := helpers.ValidateToken(userID, auth)
 
 	if isValid {
 		var users []interfaces.User
-		db 		:= helpers.ConnectDB()
+		db := helpers.ConnectDB()
 		db.Find(&users)
-		
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(users)
@@ -173,15 +182,31 @@ func updateUser(w http.ResponseWriter, r *http.Request) {
 
 	var formattedBody User
 
-	vars 	:= mux.Vars(r)
-	userID 	:= vars["id"]
-	body 	:= readBody(r)
-	err 	:= json.Unmarshal(body, &formattedBody)
+	vars := mux.Vars(r)
+	userID := vars["id"]
+	body := readBody(r)
+	err := json.Unmarshal(body, &formattedBody)
 
 	helpers.HandleErr(err)
 
 	updateUser := users.UpdateUser(userID, formattedBody.Username, formattedBody.Email, formattedBody.Password, formattedBody.UserType, formattedBody.Name, formattedBody.Balance, auth)
 	apiResponse(updateUser, w)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	userId := mux.Vars(r)["id"]
+	fmt.Println(userId)
+
+	db := helpers.ConnectDB()
+	var user User
+	db.Where("id = ?", userId).Delete(&user)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	json.NewEncoder(w).Encode("User sudah dihapus")
 }
 
 func createPost(w http.ResponseWriter, r *http.Request) {
@@ -197,10 +222,10 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 
 func readPost(w http.ResponseWriter, r *http.Request) {
 
-	vars 	:= mux.Vars(r)
-	postId 	:= vars["id"]
+	vars := mux.Vars(r)
+	postId := vars["id"]
 	auth := r.Header.Get("Authorization")
-	post 	:= posts.ReadPost(postId, auth)
+	post := posts.ReadPost(postId, auth)
 	apiResponse(post, w)
 
 }
@@ -208,14 +233,14 @@ func readPost(w http.ResponseWriter, r *http.Request) {
 func readAllPost(w http.ResponseWriter, r *http.Request) {
 
 	//TODO: Gunakan apiresponse interfasce
-	auth 	:= r.Header.Get("Authorization")	
-	userID 	:= helpers.UserIDStr(auth)
+	auth := r.Header.Get("Authorization")
+	userID := helpers.UserIDStr(auth)
 	isValid := helpers.ValidateToken(userID, auth)
 
 	if isValid == true {
 
 		var posts []interfaces.Post
-		db 		:= helpers.ConnectDB()
+		db := helpers.ConnectDB()
 		if err := db.Find(&posts).Error; err != nil {
 			RespondWithJSON(w, 400, JSONResponse{
 				Status:  "failed",
@@ -238,10 +263,10 @@ func readAllPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func deletePost(w http.ResponseWriter, r *http.Request) {
-	vars 	:= mux.Vars(r)
-	postId 	:= vars["id"]
+	vars := mux.Vars(r)
+	postId := vars["id"]
 	auth := r.Header.Get("Authorization")
-	post 	:= posts.DeletePost(postId, auth)
+	post := posts.DeletePost(postId, auth)
 	apiResponse(post, w)
 
 }
@@ -251,10 +276,10 @@ func updatePost(w http.ResponseWriter, r *http.Request) {
 
 	var formattedBody Post
 
-	vars 	:= mux.Vars(r)
-	postId 	:= vars["id"]
-	body 	:= readBody(r)
-	err 	:= json.Unmarshal(body, &formattedBody)
+	vars := mux.Vars(r)
+	postId := vars["id"]
+	body := readBody(r)
+	err := json.Unmarshal(body, &formattedBody)
 
 	helpers.HandleErr(err)
 
@@ -275,10 +300,10 @@ func createJadwal(w http.ResponseWriter, r *http.Request) {
 
 func readJadwal(w http.ResponseWriter, r *http.Request) {
 
-	vars 		:= mux.Vars(r)
-	jadwalID 	:= vars["id"]
-	auth 		:= r.Header.Get("Authorization")
-	jadwal 		:= jadwals.ReadJadwal(jadwalID, auth)
+	vars := mux.Vars(r)
+	jadwalID := vars["id"]
+	auth := r.Header.Get("Authorization")
+	jadwal := jadwals.ReadJadwal(jadwalID, auth)
 	apiResponse(jadwal, w)
 
 }
@@ -286,29 +311,29 @@ func readJadwal(w http.ResponseWriter, r *http.Request) {
 func readAllJadwal(w http.ResponseWriter, r *http.Request) {
 
 	//TODO: Gunakan apiresponse interfasce
-	auth 	:= r.Header.Get("Authorization")	
-	userID 	:= helpers.UserIDStr(auth)
+	auth := r.Header.Get("Authorization")
+	userID := helpers.UserIDStr(auth)
 	isValid := helpers.ValidateToken(userID, auth)
 
 	if isValid {
 
 		var jadwals []interfaces.Jadwal
-		db 		:= helpers.ConnectDB()
+		db := helpers.ConnectDB()
 		db.Find(&jadwals)
-		
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(jadwals)
-		
+
 	}
 
 }
 
 func deleteJadwal(w http.ResponseWriter, r *http.Request) {
-	vars 		:= mux.Vars(r)
-	jadwalID 	:= vars["id"]
-	auth 		:= r.Header.Get("Authorization")
-	jadwal 		:= jadwals.DeleteJadwal(jadwalID, auth)
+	vars := mux.Vars(r)
+	jadwalID := vars["id"]
+	auth := r.Header.Get("Authorization")
+	jadwal := jadwals.DeleteJadwal(jadwalID, auth)
 	apiResponse(jadwal, w)
 
 }
@@ -318,10 +343,10 @@ func updateJadwal(w http.ResponseWriter, r *http.Request) {
 
 	var formattedBody Jadwal
 
-	vars 		:= mux.Vars(r)
-	jadwalID	:= vars["id"]
-	body 		:= readBody(r)
-	err 		:= json.Unmarshal(body, &formattedBody)
+	vars := mux.Vars(r)
+	jadwalID := vars["id"]
+	body := readBody(r)
+	err := json.Unmarshal(body, &formattedBody)
 
 	helpers.HandleErr(err)
 
@@ -339,6 +364,7 @@ func StartApi() {
 	router.HandleFunc("/users", readAllUser).Methods("GET")
 	router.HandleFunc("/user/{id}", getUser).Methods("GET")
 	router.HandleFunc("/user/{id}", updateUser).Methods("PUT")
+	router.HandleFunc("/user/{id}", deleteUser).Methods("DELETE")
 
 	//POST
 	router.HandleFunc("/post", createPost).Methods("POST")
